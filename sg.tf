c597ee8a53abfd9cb48c0677f14ae38fac401fc8
paramdl <dlprashanth22@gmail.com>
2023-02-20 17:20:55 +0530

    commit

---
+++ sg.tf
@@ -0,0 +1,45 @@
+resource "aws_security_group" "vpc_conf" {
+name = "vpc_conf"
+vpc_id = "************"
+
+ingress {
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+from_port = 22
+    to_port = 22
+    protocol = "tcp"
+  }
+ingress {
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+from_port = 443
+    to_port = 443
+    protocol = "tcp"
+  }
+ingress {
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+from_port = 80
+    to_port = 80
+    protocol = "tcp"
+  }
+ingress {
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+  }
+  egress {
+   from_port = 0
+ to_port = 0
+   protocol = "-1"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
+}
+                           
+
---
+++ terraform_final.tf
@@ -0,0 +1,102 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
+provider "aws"{
+access_key= "*******"
+secret_key= "*****"
+region= "ap-south-1"
+}
+
+resource "aws_instance" "Tomcat_conf" {
+  ami           = "ami-0e742cca61fb65051"
+  instance_type = "t2.micro"
+key_name= "******"
+vpc_security_group_ids      = ["${aws_security_group.vpc_conf.id}"]
+provisioner "file" {
+    source      = "conf.sh"
+    destination = "conf.sh"
+  }
+provisioner "remote-exec"  {
+    inline  = [
+    "sudo yum install -y java",
+    "sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.71/bin/apache-tomcat-9.0.71.tar.gz",
+    "sudo yum upgrade -y",
+    "sudo tar -xf apache-tomcat-9.0.71.tar.gz",
+    "sudo sh conf.sh",
+    ]
+}
+connection {
+    type         = "ssh"
+        host         = self.public_ip
+            user         = "ec2-user"
+                private_key  = file("/root/task1/*****" )
+                   }
+  tags = {
+    "Name" = "Tomcat_conf"
+  }
+}
+
+
+resource "aws_instance" "Jenkins" {
+  ami           = "ami-0e742cca61fb65051"
+  instance_type = "t2.micro"
+key_name= "aws"
+vpc_security_group_ids      = ["${aws_security_group.vpc_conf.id}"]
+
+provisioner "remote-exec"  {
+    inline  = [
+      "sudo yum install -y jenkins java-11-openjdk-devel",
+      "sudo yum -y install java",
+      "sudo yum -y install wget",
+      "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo",
+      "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key",
+      "sudo yum upgrade -y",
+      "sudo yum install jenkins -y",
+      "sudo systemctl enable jenkins",
+      "sudo systemctl start jenkins",
+      "sudo yum install git -y",
+      ]
+   }
+
+connection {
+    type         = "ssh"
+        host         = self.public_ip
+            user         = "ec2-user"
+                private_key  = file("/root/task1/*****" )
+                   }
+  tags = {
+    "Name" = "Jenkins_conf"
+  }
+}
+
+
+resource "aws_instance" "Jira" {
+  ami           = "ami-0e742cca61fb65051"
+  instance_type = "t2.medium"
+key_name= "aws"
+vpc_security_group_ids      = ["${aws_security_group.vpc_conf.id}"]
+provisioner "remote-exec"  {
+    inline  = [
+      "sudo yum update -y",
+      "sudo wget https://www.atlassian.com/software/jira/downloads/binary/atlassian-jira-software-9.6.0-x64.bin",
+      "sudo chmod +x atlassian-jira-software-9.6.0-x64.bin",
+   ]
+   }
+connection {
+    type         = "ssh"
+        host         = self.public_ip
+            user         = "ec2-user"
+                private_key  = file("/root/task1/****" )
+                   }
+  tags = {
+    "Name" = "Jira_conf"
+  }
+}
+
+
+     
+   
